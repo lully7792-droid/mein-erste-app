fde5c5b1f0ffd84748794d4b0b3caf64da7714ea
lully7792-droid <lully7792@gmail.com>
2026-06-13 16:54:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -290,4 +290,4 @@ app.post('/api/radar-hunt', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server läuft fehlerfrei auf Port ${PORT}`);
-})
+});
